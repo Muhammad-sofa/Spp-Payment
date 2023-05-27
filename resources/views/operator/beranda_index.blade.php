8bc70175fcfefe63207e5a3e39538967ef240812
Sofa <muhammadsofa52@yahoo.com>
2023-05-27 11:33:05 +0700

    Fix Minor

--- resources/views/operator/beranda_index.blade.php
+++ resources/views/operator/beranda_index.blade.php
@@ -1,11 +1,9 @@
-@extends('layouts.app')
-
+@extends('layouts.app_sneat')
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <h5 class="card-header">Hi Operator</h5>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -19,5 +17,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
