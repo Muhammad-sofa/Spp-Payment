ff1e1157bfc2fefba85f232ecedfa4f298a23258
Sofa <muhammadsofa52@yahoo.com>
2023-06-11 08:35:39 +0700

    Use Checkbox

--- resources/views/operator/tagihan_form.blade.php
+++ resources/views/operator/tagihan_form.blade.php
@@ -9,12 +9,23 @@
                          'route' => $route, 
                          'method' => $method,
                     ]) !!}
-                    <div class="form-group mt-1">
+                    {{-- <div class="form-group mt-1">
                          <label for="biaya_id">Biaya Yang Ditagihkan</label>
-                         {!! Form::select('biaya_id', $biaya, null, ['class' => 'form-control', 'multiple' => true]) !!}
+                         {!! Form::select('biaya_id[]', $biaya, null, ['class' => 'form-control', 'multiple' => true]) !!}
                          <span class="text-danger">{{ $errors->first('biaya_id') }}</span>
+                    </div> --}}
+                    @foreach ($biaya as $item)
+                    <div class="form-check mt-3">
+                         {!! Form::checkbox('biaya_id[]', $item->id, null, [
+                              'class' => 'form-check-input',
+                              'id' => 'defaultCheck' . $loop->iteration,
+                              ]) !!}
+                         <label class="form-check-label" for="defaultCheck{{ $loop->iteration }}">
+                              {{ $item->nama_biaya_full}}
+                         </label>
                     </div>
-                    <div class="form-group mt-1">
+                    @endforeach
+                    <div class="form-group mt-3">
                          <label for="angkatan">Tagihan Untuk Angkatan</label>
                          {!! Form::select('angkatan', $angkatan, null, ['class' => 'form-control', 'placeholder' => 'Pilih Angkatan']) !!}
                          <span class="text-danger">{{ $errors->first('angkatan') }}</span>
