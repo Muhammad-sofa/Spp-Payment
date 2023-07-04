58d81e61bb3e62dc4515c8b8fd556c7e342f3a21
Sofa <muhammadsofa52@yahoo.com>
2023-07-04 22:43:17 +0700

    Update Form Pembayaran Wali Murid 2

--- resources/views/wali/pembayaran_form.blade.php
+++ resources/views/wali/pembayaran_form.blade.php
@@ -8,9 +8,31 @@
                     {!! Form::model($model, ['route' => $route, 'method' => $method]) !!}
                     <div class="form-group">
                          <label for="bank_id">Bank Tujuan Pembayaran</label>
-                         {!! Form::select('bank_id', $listBank, request('bank_sekolah_id'), ['class' => 'form-control ']) !!}
+                         {!! Form::select('bank_id', $listBank, request('bank_sekolah_id'), ['class' => 'form-control', 'placeholder' => 'Pilih Bank Tujuan Transfer']) !!}
                          <span class="text-danger">{{ $errors->first('bank_id') }}</span>
                     </div>
+
+                    @if(request('bank_sekolah_id') != '')
+                    <div class="alert alert-primary mt-3" role="alert">
+                         <table width="100%">
+                              <tbody>
+                                   <tr>
+                                        <td width="15%">Bank Tujuan</td>
+                                        <td>: {{ $bankYangDipilih->nama_bank }}</td>
+                                   </tr>
+                                   <tr>
+                                        <td>Nomor Rekening</td>
+                                        <td>: {{ $bankYangDipilih->nomor_rekening }}</td>
+                                   </tr>
+                                   <tr>
+                                        <td>Atas Nama</td>
+                                        <td>: {{ $bankYangDipilih->nama_rekening }}</td>
+                                   </tr>
+                              </tbody>
+                         </table>
+                    </div>
+
+                    @endif
 
                     <div class="form-group mt-3">
                          <label for="tanggal_bayar">Tanggal Bayar</label>
