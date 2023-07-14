1560c04ccec8dc3cb5bff9e506533ab982395631
Sofa <muhammadsofa52@yahoo.com>
2023-07-14 10:08:05 +0700

    Validasi Pembayaran Wali Murid

--- app/Http/Controllers/WaliMuridPembayaranController.php
+++ app/Http/Controllers/WaliMuridPembayaranController.php
@@ -66,12 +66,26 @@ class WaliMuridPembayaranController extends Controller
             $waliBankId = $request->wali_bank_id;
             $waliBank = WaliBank::findOrFail($waliBankId);
         }
+
+        $jumlahDibayar = str_replace('.', '', $request->jumlah_dibayar);
+
+        $validasiPembayaran = Pembayaran::where('jumlah_dibayar', $jumlahDibayar)
+            ->where('tagihan_id', $request->tagihan_id)
+            ->where('status_konfirmasi', 'belum')
+            ->first();
+        if ($validasiPembayaran != null) {
+            flash('Data pembayaran ini sudah ada dan akan dikonfirmasi oleh operator');
+            return back();
+        }
+
         $request->validate([
             'tanggal_bayar' => 'required',
             'jumlah_dibayar' =>'required',
             'bukti_bayar' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5048',
         ]);
+
         $buktiBayar = $request->file('bukti_bayar')->store('public');
+
         $dataPembayaran = [
             'bank_sekolah_id' => $request->bank_sekolah_id,
             'wali_bank_id' => $waliBank->id,
@@ -79,7 +93,7 @@ class WaliMuridPembayaranController extends Controller
             'wali_id' => auth()->user()->id,
             'tanggal_bayar' => $request->tanggal_bayar,
             'status_konfirmasi' => 'belum',
-            'jumlah_dibayar' => str_replace('.', '', $request->jumlah_dibayar),
+            'jumlah_dibayar' => $jumlahDibayar,
             'bukti_bayar' => $buktiBayar,
             'metode_pembayaran' => 'transfer',
             'user_id' => 0,
